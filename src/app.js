f7f06bbf506671c03ae0f4ac318b9a89c0bbb6a0
Jung Jaewoo <jaewo7o@naver.com>
2021-12-04 20:59:17 +0900

    second commit

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import './app.css'
 import webpackImage from './webpack.png'
 
 window.addEventListener('DOMContentLoaded', () => {
-    const result = sum(1, 2)
+    const param1 = 1
+    const param2 = 3
+    const result = sum(param1, param2)
     const el = document.querySelector('#app')
-    el.innerHTML = `<h1> result : ${result} </h1>
+    el.innerHTML = `<h1> ${param1} + ${param2} : ${result} </h1>
     <img src="${webpackImage}" alt="webpack image" />
     `
 })
