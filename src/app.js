7f1e4eb9fba9f633ecc769321f7c836c7b413b45
Jung Jaewoo <jaewo7o@naver.com>
2021-12-04 21:01:33 +0900

    second commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import './app.css'
 import webpackImage from './webpack.png'
 
 window.addEventListener('DOMContentLoaded', () => {
-    const param1 = 1
+    const param1 = 2
     const param2 = 3
     const result = sum(param1, param2)
     const el = document.querySelector('#app')
